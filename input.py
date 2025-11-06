bb8ea333337a974e2ed431eda6b9cdccee362944
DominikWiechert <dominik-wiechert@freenet.de>
2025-11-06 15:27:20 +0100

    Exchange heuristic to determine color
    - Use euclidean distance in CIELUV color representation

--- input.py
+++ input.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 from custom_datatypes import MapNode, LandUse
 from pathlib import Path
@@ -18,7 +19,6 @@ def get_forest_map(map_path: Path = None, max_axis_length: int = 200) -> List[Li
            V
        y (height)
     """
-    #
     input_image = cv2.imread(str(map_path))
 
     # Downscale image, maintain ratios
@@ -31,21 +31,35 @@ def get_forest_map(map_path: Path = None, max_axis_length: int = 200) -> List[Li
         grid_height = max_axis_length
         grid_width = int(ratio * grid_height)
     input_image = cv2.resize(input_image,(grid_width, grid_height))
+    input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2Luv)
+    forest_color = get_forest_color_LUV()
+    [l_forest, u_forest, v_forest] = forest_color[0], forest_color[1], forest_color[2]
 
-    # Read RGB and separate into terrain types. Generate grid
-    grid = []
+    output_grid = []
     for i_height in range(input_image.shape[0]):
         row = []
         for i_width in range(input_image.shape[1]):
-            r, g, b = input_image[i_height, i_width]
-            # TODO: Differentiate between different green tones. (e.g. light green is just a field, dark green is a forest). Might use a different colour representation (e.g. HSL)
-            if g > r + 30 and g > b + 10:
+            l_pixel, u_pixel, v_pixel = input_image[i_height, i_width]
+            d_pixel2forest = math.sqrt((int(l_pixel) - int(l_forest))**2 +
+                                       (int(u_pixel) - int(u_forest))**2 +
+                                       (int(v_pixel) - int(v_forest))**2)
+            if d_pixel2forest < 40:
                 row.append(MapNode(land_use=LandUse.FOREST, height=0))
             else:
                 row.append(MapNode(land_use=LandUse.MISC, height=0))
-        grid.append(row)
+        output_grid.append(row)
+    return output_grid
 
-    return grid
+
+def get_forest_color_LUV() -> List[int]:
+    forest_image_path = Path().cwd() / 'map_pictures' / 'only_forest.png'
+
+    forest_image = cv2.imread(str(forest_image_path))
+    forest_image = cv2.resize(forest_image, (1, 1))
+    forest_image = cv2.cvtColor(forest_image, cv2.COLOR_BGR2Luv)
+
+    l, u, v = forest_image[0, 0]
+    return [l, u, v]
 
 
 if __name__ == "__main__":
