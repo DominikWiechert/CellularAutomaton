b1d1e4a87ce244e21ff197360c06f9fad574cc6f
DominikWiechert <dominik-wiechert@freenet.de>
2026-01-06 15:02:02 +0100

    Interpolate heights in map using Kriging

--- input.py
+++ input.py
@@ -1,9 +1,13 @@
 import math
 from typing import List
+
+from matplotlib import pyplot as plt
+
 from custom_datatypes import MapNode, LandUse
 from pathlib import Path
 import cv2
-
+import numpy as np
+from pykrige.ok import OrdinaryKriging
 
 def get_forest_map(map_path: Path = None, max_axis_length: int = 200) -> List[List[MapNode]]:
     """
@@ -35,18 +39,53 @@ def get_forest_map(map_path: Path = None, max_axis_length: int = 200) -> List[Li
     forest_color = get_forest_color_luv()
     [l_forest, u_forest, v_forest] = forest_color[0], forest_color[1], forest_color[2]
 
+    # Example height datapoints
+    # TODO: Input height datapoints
+    data_heights_x = np.array([10, 20, grid_width-1, 80], dtype=float)
+    data_heights_y = np.array([10, 60, grid_height-1, 75], dtype=float)
+    data_heights = np.array([100, 50, 200, 20], dtype=float)
+
+    # Grid for interpolation
+    grid_x = np.arange(0, grid_width, 1, dtype=float)
+    grid_y = np.arange(0, grid_height, 1, dtype=float)
+
+    # Ordinary Kriging
+    OK = OrdinaryKriging(
+        data_heights_x,
+        data_heights_y,
+        data_heights,
+        variogram_model='spherical',
+        verbose=False,
+        enable_plotting=False
+    )
+
+    # Interpolation
+    interpolated_heights, ss = OK.execute('grid', grid_x, grid_y)
+
+    show_height_graph = False
+    if show_height_graph:
+        plt.figure(figsize=(6, 5))
+        plt.contourf(grid_x, grid_y, interpolated_heights, cmap='viridis')
+        plt.scatter(data_heights_x, data_heights_y, c=data_heights, edgecolor='k', cmap='viridis')
+        plt.colorbar(label='Interpolierte Höhe')
+        plt.title("Ordinary Kriging – Interpolated Heights")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.show()
+
     output_grid = []
     for i_height in range(input_image.shape[0]):
         row = []
         for i_width in range(input_image.shape[1]):
+            pixel_height = interpolated_heights[i_height][i_width]
             l_pixel, u_pixel, v_pixel = input_image[i_height, i_width]
             d_pixel2forest = math.sqrt((int(l_pixel) - int(l_forest))**2 +
                                        (int(u_pixel) - int(u_forest))**2 +
                                        (int(v_pixel) - int(v_forest))**2)
             if d_pixel2forest < 40:
-                row.append(MapNode(land_use=LandUse.FOREST, height=0))
+                row.append(MapNode(land_use=LandUse.FOREST, height=pixel_height))
             else:
-                row.append(MapNode(land_use=LandUse.MISC, height=0))
+                row.append(MapNode(land_use=LandUse.MISC, height=pixel_height))
         output_grid.append(row)
     return output_grid
 
